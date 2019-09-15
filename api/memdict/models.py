a763ad792e97086abbe02fbbe406ab3f157da3fe
kareem <kareem@k.com>
2019-09-15 04:04:46 +0200

    changes settings and url and adds models

--- api/memdict/models.py
+++ api/memdict/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Languages(models.Model):
+	name = models.CharField(max_length=255, null=False)
+	def __str__(self):
+		return name
+
+class Words(models.Model):
+	language = models.ForeignKey(Languages, on_delete=models.CASCADE)
+	word = models.CharField(max_length=255, nill=False)
+	def __str__(self):
+		return word
+
+class Meanings(models.Model):
+	language = models.ForeignKey(Languages, on_delete=models.CASCADE)
+	meaning = models.CharField(max_length=255, nill=False)
+	def __str__(self):
+		return meaning
+
+class Mnomonics(models.Model):
+	language = models.ForeignKey(Languages, on_delete=models.CASCADE)
+	word = models.ForeignKey(Words, on_delete=models.CASCADE)
+	mnomonic = models.CharField(max_length=255, nill=False)
+	def __str__(self):
+		return mnomonic
+
+
